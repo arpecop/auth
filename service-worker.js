6b9ee128ec8b02c0edfa2919b63010b1b7ccfc9a
arpecop <arpecop@users.noreply.github.com>
2020-10-25 07:15:48 +0000

    deploy: 78f55c9e25a456bf46c2d8ab44e544e967b5f74d

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2851e0be7172808dc10c39539b216d20.js"
+  "/precache-manifest.976fb71c2d71b2742d639476e6c1849c.js"
 );
 
 self.addEventListener('message', (event) => {
