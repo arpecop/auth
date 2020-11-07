6337aa9ffb2b2564d5d30e63aa202aaafaef6cb3
Radoslav <rudix.lab@gmail.com>
2020-11-07 07:25:07 +0200

    c

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,22 +1,23 @@
 import React, { useEffect, useState } from 'react';
 
 import { useRecoilState } from 'recoil';
-import { get } from '../utils/api';
+import { postPublic } from '../utils/api';
 import { navigation } from '../utils/state';
 
 const Welcome = ({ menu }) => {
   const [nav, setNav] = useRecoilState(navigation);
-  const [fields, setFields] = useState({ rows: [] });
+  const [fields, setFields] = useState({ Items: [] });
   useEffect(() => {
     setNav(menu);
   }, [setNav, nav, menu]);
 
   useEffect(() => {
     async function fetchData() {
-      const data = await get(
-        '/test/_design/api/_view/feedAll?reduce=false&descending=true',
-      );
-      setFields(data.data);
+      const response = await postPublic({
+        collection: 'test1-all',
+        descending: false,
+      });
+      setFields(response.data);
     }
     fetchData();
   }, []);
@@ -24,13 +25,10 @@ const Welcome = ({ menu }) => {
   return (
     <div>
       <h2>Public Feed</h2>
-      {fields.rows.map((item) => (
-        <div key={item.id}>
-
-          <small>
-            {`${item.value.username ? item.value.username : 'admin'} added: `}
-          </small>
-          {item.value.task}
+      {fields.Items.map((item) => (
+        <div key={item.vreme}>
+          <small>{`${item.username ? item.username : 'admin'} added: `}</small>
+          {item.task}
         </div>
       ))}
     </div>
--- src/utils/api.js
+++ src/utils/api.js
@@ -2,10 +2,10 @@ import axios from 'axios';
 
 const env1 = {
   development: {
-    api: 'https://db.rudixlab.com',
+    api: 'http://localhost:3001',
   },
   production: {
-    api: 'https://db.rudixlab.com',
+    api: 'https://47ruk9bopl.execute-api.eu-central-1.amazonaws.com/dev',
   },
 };
 export const env = env1[process.env.NODE_ENV];
@@ -50,7 +50,7 @@ export const postPublic = async (json) => {
   });
 };
 export const put = async (json, token) => {
-  const result = await axios.post(`${env.api}/db/test`, JSON.stringify(json), {
+  const result = await axios.post(`${env.api}/insert`, JSON.stringify(json), {
     headers: {
       accesstoken: token,
       'Content-Type': 'application/json',
@@ -60,8 +60,8 @@ export const put = async (json, token) => {
     resolve(result);
   });
 };
-export const del = async (id, token) => {
-  const result = await axios.get(`${env.api}/del/test/${id}`, {
+export const del = async (json, token) => {
+  const result = await axios.post(`${env.api}/del`, JSON.stringify(json), {
     headers: {
       accesstoken: token,
       'Content-Type': 'application/json',
